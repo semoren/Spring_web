e111fe5976603d9348b1d65f816d79f1c5e72ba3
zhangzhenzhou <zhangzhenzhou@zzz-pc>
2016-05-10 18:37:00 +0800

    修改userinfo中变量名

--- src/main/java/com/sermo/xx/model/UserInfo.java
+++ src/main/java/com/sermo/xx/model/UserInfo.java
@@ -10,7 +10,7 @@ public class UserInfo  implements Serializable{
 
 	private String email;
 	
-	private String name;
+	private String userName;
 	
 	private String password;
 	
@@ -24,12 +24,12 @@ public class UserInfo  implements Serializable{
 		this.email = email;
 	}
 
-	public String getName() {
-		return name;
+	public String getUserName() {
+		return userName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 
 	public String getPassword() {
